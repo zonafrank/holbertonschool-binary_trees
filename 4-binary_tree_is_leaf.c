23317780745eb2798d9f2dd8eb86b6d69fe19964
Eche <echyth@yahoo.com>
2024-01-13 08:51:46 +1000

    fix: style code errors in task 4 function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,10 +9,10 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 
 	if (node->left == NULL && node->right == NULL)
-		return 1;
+		return (1);
 
-	return 0;
+	return (0);
 }
