a75df02c17a1342a54d48adb15a24983afb94ded
Eche <echyth@yahoo.com>
2024-01-12 21:47:07 +1000

    fix: updates for coding style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+
 	if (parent == NULL)
 		return NULL;
-
-	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
 
