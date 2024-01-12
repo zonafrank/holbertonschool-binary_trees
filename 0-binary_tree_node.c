4365f48a5ed80fd9d855c75f888aba13f9019683
Eche <echyth@yahoo.com>
2024-01-12 15:39:24 +1000

    doc: add documentation for binary_tree_node func

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * @brief Creates a new binary tree node
+ *
+ * @param parent Pointer to the parent node of the new node
+ * @param value Value to be stored in the new node
+ *
+ * @return Pointer to the newly created node, or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
