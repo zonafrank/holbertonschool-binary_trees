d5203d83cdde01d37a90ff2a820512ea6bd1b078
Eche <echyth@yahoo.com>
2024-01-12 21:51:35 +1000

    fix: missing prototype in header

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,12 +12,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return NULL;
+
 	new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
+		return (NULL);
 	}
 
 	new->n = value;
