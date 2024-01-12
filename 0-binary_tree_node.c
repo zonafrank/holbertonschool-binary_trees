4b9528653933d3c15d391c2ab06c213bab2016a6
Eche <echyth@yahoo.com>
2024-01-12 15:36:40 +1000

    updates for initial implementation of func binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,5 +2,20 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	// TODO
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return new;
 }
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
