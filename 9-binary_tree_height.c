4c0800bb0f221e888866917a8c042d1da18fffd2
Eche <echyth@yahoo.com>
2024-01-13 10:08:02 +1000

    feature: task 9 initial commit

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of tree or zero if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	if (left_height == (size_t)-1)
+		return (size_t)-1;
+
+	right_height = binary_tree_height(tree->right);
+	if (right_height == (size_t)-1)
+		return (size_t)-1;
+
+	if (left_height >= right_height)
+		return left_height + 1;
+	else
+		return right_height + 1;
+}
