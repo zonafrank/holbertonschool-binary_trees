9c6f142c16421a029b31661915355196cb8424fd
Eche <echyth@yahoo.com>
2024-01-13 10:13:13 +1000

    fix: task 9 - return 0 for node with no children

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right)
 		return (0);
 
+	left_height = binary_tree_height(tree->left);
+	if (left_height == (size_t)-1)
+		return (size_t)-1;
+
+	right_height = binary_tree_height(tree->right);
+	if (right_height == (size_t)-1)
+		return (size_t)-1;
+
 	if (left_height >= right_height)
 		return left_height + 1;
 	else
