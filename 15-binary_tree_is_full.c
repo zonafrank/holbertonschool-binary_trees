df37a00c22b7413e683ced6c6c54a450daa6b921
Eche <echyth@yahoo.com>
2024-01-13 14:14:49 +1000

    fix: task 15 - update for failing tests

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,6 +16,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		(tree->left != NULL && tree->right == NULL))
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return 1;
+
 	left_is_full = binary_tree_is_full(tree->left);
 	right_is_full = binary_tree_is_full(tree->right);
 	return left_is_full && right_is_full;
