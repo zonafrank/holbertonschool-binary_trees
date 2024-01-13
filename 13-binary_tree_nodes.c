269836cfd95fa6eabf007c494e4adecaf20e2efe
Eche <echyth@yahoo.com>
2024-01-13 11:30:16 +1000

    fix: task 13 - updates for failing tests

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,16 +7,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t left_nodes;
-	size_t right_nodes;
+	size_t left_nodes, right_nodes;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
-		return (1);
-
 	left_nodes = binary_tree_nodes(tree->left);
 	right_nodes = binary_tree_nodes(tree->right);
+
+	if (tree->left != NULL && tree->right != NULL)
+		return (1 + left_nodes + right_nodes);
+
 	return (left_nodes + right_nodes);
 }
